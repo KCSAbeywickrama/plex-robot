9656ac923578ad3c90c9d3e2421257a5968aa4f1
CSA <abeywickramakcs.19@uom.lk>
2022-02-17 08:00:27 +0530

    add imageGradient to vision lib

--- Simulation/CV/cvtest3/controllers/testing_controller/plexlibs/vision.cpp
+++ Simulation/CV/cvtest3/controllers/testing_controller/plexlibs/vision.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include "vision.hpp"
 
-using namespace std;
 namespace vision
 {
     void gotoObject()
@@ -12,4 +10,21 @@ namespace vision
     {
         cout << "vision:mainFunction()" << endl;
     }
+    void imageGradient(Mat img, int width, int height, int &gi, int &gj)
+    {
+        gi = 0;
+        gj = 0;
+
+        for (int i = 0; i < height - 1; i++)
+        {
+            uchar *line0 = img.ptr<uchar>(i);
+            uchar *line1 = img.ptr<uchar>(i + 1);
+            for (int j = 0; j < width - 1; j++)
+            {
+                gi += line1[j] - line0[j];
+                gj += line0[j + 1] - line0[j];
+            }
+        }
+    }
+
 }
--- Simulation/CV/cvtest3/controllers/testing_controller/plexlibs/vision.hpp
+++ Simulation/CV/cvtest3/controllers/testing_controller/plexlibs/vision.hpp
@@ -1,4 +1,9 @@
 #pragma once
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 namespace vision
 {
