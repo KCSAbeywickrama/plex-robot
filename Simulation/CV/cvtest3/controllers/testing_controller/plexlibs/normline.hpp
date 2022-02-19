fbe37189d9defd6feb03ee18e6af48b25d77e491
CSA <abeywickramakcs.19@uom.lk>
2022-02-19 18:03:49 +0530

    add normline dashline libfiles

---
+++ Simulation/CV/cvtest3/controllers/testing_controller/plexlibs/normline.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+#define TIME_STEP 16
+#define NORM_MAX 6.28
+#define NORM_BASE 4
+
+using namespace webots;
+using namespace std;
+
+namespace normline
+{
+    void init(Robot *robot);
+    void follow(Robot *robot);
+}
