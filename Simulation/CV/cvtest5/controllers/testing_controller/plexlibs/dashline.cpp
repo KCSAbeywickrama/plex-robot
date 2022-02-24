4c98a0fd46fc65af44e147c64392601590dd27f8
CSA <abeywickramakcs.19@uom.lk>
2022-02-25 03:28:59 +0530

    add _redball to init in dashline

--- Simulation/CV/cvtest5/controllers/testing_controller/plexlibs/dashline.cpp
+++ Simulation/CV/cvtest5/controllers/testing_controller/plexlibs/dashline.cpp
@@ -2,7 +2,7 @@
 
 namespace dashline
 {
-  double kp =4;
+  double kp = 4;
   double ki = 0.001;
   double kd = 0.2;
   double p = 0;
@@ -23,21 +23,22 @@ namespace dashline
   int act = 0;
   double values[8];
   double value[8];
-  double value_max=0;
-  double value_min=1000;  
+  double value_max = 0;
+  double value_min = 1000;
   int redBall = 0;
   bool end = 0;
   DistanceSensor *sensors[8];
-  const char* sensor_names[8]={"s0","s1","s2","s3","s4","s5","s6","s7"};
+  const char *sensor_names[8] = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7"};
 
   Motor *lm;
   Motor *rm;
 
-  void init(Robot *robot)
+  void init(Robot *robot, int _redBall)
   {
-    for(int i=0;i<8;i++){
-    sensors[i]=robot->getDistanceSensor(sensor_names[i]);
-    sensors[i]->enable(TIME_STEP);
+    for (int i = 0; i < 8; i++)
+    {
+      sensors[i] = robot->getDistanceSensor(sensor_names[i]);
+      sensors[i]->enable(TIME_STEP);
     }
     lm = robot->getMotor("leftMotor");
     rm = robot->getMotor("rightMotor");
@@ -45,42 +46,42 @@ namespace dashline
     lm->setPosition(INFINITY);
     rm->setPosition(INFINITY);
     lm->setVelocity(0);
-    rm->setVelocity(0);   
-	//redBall = _redBall;
+    rm->setVelocity(0);
+    redBall = _redBall;
   }
-  
+
   void sensor_check()
   {
-    value_max=0;
-    value_min=1000;  
+    value_max = 0;
+    value_min = 1000;
     act = 0;
-    for(int i =0; i<8;i++){
-    value[i]=(sensors[i]->getValue());
-    std::cout << 2.53268-1.23521*pow(value[i],0.239678) << std::endl;
-    if(value[i]>value_max){
-    value_max=value[i];
+    for (int i = 0; i < 8; i++)
+    {
+      value[i] = (sensors[i]->getValue());
+      std::cout << 2.53268 - 1.23521 * pow(value[i], 0.239678) << std::endl;
+      if (value[i] > value_max)
+      {
+        value_max = value[i];
+      }
+      if (value[i] < value_min)
+      {
+        value_min = value[i];
+      }
     }
-    if(value[i]<value_min){
-    value_min=value[i];
+    if (abs(value_min - value_max) > 0.005)
+    {
+      t = (2.53268 - 1.23521 * pow(value_min, 0.239678) + 2.53268 - 1.23521 * pow(value_max, 0.239678)) / 2;
     }
-    
-    }
-    if(abs(value_min-value_max)>0.005){
-    t=(2.53268-1.23521*pow(value_min,0.239678)+2.53268-1.23521*pow(value_max,0.239678))/2;
-    }
-    
-    std::cout << "**************" << std::endl;
-    std::cout <<"t = "<< t << std::endl;
-    std::cout << value_min<< std::endl;
-    std::cout << value_max<< std::endl;
-    std::cout << "--------------" << std::endl;
-    
-    
-    
-    for(int i =0; i<8;i++){
-    values[i] = 1- int((float)((2.53268-1.23521*pow(value[i],0.239678)) / t));
-    //std::cout << values[i] << std::endl;
-    act+=values[i];
+
+    std::cout << "t:" << t << std::endl;
+    std::cout << "value_min:" << value_min << std::endl;
+    std::cout << "value_max:" << value_max << std::endl;
+
+    for (int i = 0; i < 8; i++)
+    {
+      values[i] = 1 - int((float)((2.53268 - 1.23521 * pow(value[i], 0.239678)) / t));
+      // std::cout << values[i] << std::endl;
+      act += values[i];
     }
     /*std::cout << "**************" << std::endl;
     std::cout << t << std::endl;
@@ -95,54 +96,57 @@ namespace dashline
 
   void endcheck(Robot *robot)
   {
-  if(act>5){
-  et+=1;
-  //std::cout << et << std::endl;
-  etime=robot->getTime();
-  }
-  else if((robot->getTime()-etime)>0.2 && act < 5){
-  et=0;
-  }
-  while(robot->step(TIME_STEP) != -1 && (robot->getTime()-etime)<0.1 && etime!=0 && et>6 && act<8){
-  L_speed=PATH_BASE_SPEED;
-  R_speed=PATH_BASE_SPEED;
-  std::cout << "**************" << std::endl;
-  speedset();
-  sensor_check();
-  if (act==0){
-  et=0;
-  end=1;
-  }
-  }
+    if (act > 5)
+    {
+      et += 1;
+      // std::cout << et << std::endl;
+      etime = robot->getTime();
+    }
+    else if ((robot->getTime() - etime) > 0.2 && act < 5)
+    {
+      et = 0;
+    }
+    while (robot->step(TIME_STEP) != -1 && (robot->getTime() - etime) < 0.1 && etime != 0 && et > 6 && act < 8)
+    {
+      L_speed = PATH_BASE_SPEED;
+      R_speed = PATH_BASE_SPEED;
+      std::cout << "**************" << std::endl;
+      speedset();
+      sensor_check();
+      if (act == 0)
+      {
+        et = 0;
+        end = 1;
+      }
+    }
   }
 
-
-void T_check(Robot *robot)
+  void T_check(Robot *robot)
   {
     if (values[7] == 1 && values[0] == 0 && (robot->getTime() - ttime) < 0.1)
-    {      
-        tc += 1;
-        if (tc > 2 && redBall)
+    {
+      tc += 1;
+      if (tc > 2 && redBall)
+      {
+        speedset();
+        tc = 0;
+        while (robot->step(TIME_STEP) != -1 && values[7] == 1)
         {
-  speedset();
-          tc = 0;
-            while (robot->step(TIME_STEP) != -1 && values[7] == 1)
-            {
-              sensor_check();
-              L_speed = 0;
-              R_speed = PATH_BASE_SPEED;
-              speedset();
-            }
+          sensor_check();
+          L_speed = 0;
+          R_speed = PATH_BASE_SPEED;
+          speedset();
         }
       }
-      else
-      {
-        tc = 0;
-      }
-      ttime = robot->getTime();
+    }
+    else
+    {
+      tc = 0;
+    }
+    ttime = robot->getTime();
   }
 
-void pid()
+  void pid()
   {
     error = 0;
     pos = 0;
@@ -154,16 +158,17 @@ void pid()
       }
       error = (4.5 - ((float)pos / act));
     }
-    if(act == 0 || act > 3){
+    if (act == 0 || act > 3)
+    {
       error = 0;
     }
-    
+
     p = error;
     i = i + error;
     d = error - lasterror;
     lasterror = error;
     speed = kp * p + ki * i + kd * d;
-       
+
     L_speed = PATH_BASE_SPEED + speed;
     R_speed = PATH_BASE_SPEED - speed;
 
@@ -186,26 +191,24 @@ void pid()
     speedset();
   }
 
-
   void follow(Robot *robot)
   {
-      while (robot->step(TIME_STEP) != -1)
+    while (robot->step(TIME_STEP) != -1)
+    {
+      if (end)
       {
-        if (end)
-        {
-          L_speed = 0;
-          R_speed = 0;
-          speedset();
-          break;
-        }
-        else
-        {
-          sensor_check();
-          T_check(robot);
-          pid();
-          endcheck(robot);
-        }
+        L_speed = 0;
+        R_speed = 0;
+        speedset();
+        break;
+      }
+      else
+      {
+        sensor_check();
+        T_check(robot);
+        pid();
+        endcheck(robot);
       }
     }
+  }
 }
-//pp
